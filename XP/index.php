4211918d5e81a49be374e4d2a45a26e7b893b2ae
fernandonesi <fernandonesi@gmail.com>
2014-06-15 21:42:18 -0300

    Incluso projeto de XP

---
+++ XP/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="img/uffs.ico">
+
+    <title>Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/login.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  	<div class="tooper" id="id-tooper">
+		<div class="logo"><img src="img/logoUFFS.png" alt="logoUFFS" height="42"/></div>
+    </div>
+	<p>Usuario: admin</p>
+    
+    <div class="container">
+    <kbd>Usuario: admin</kbd>
+    <kbd>Senha: admin</kbd>
+
+      <form class="form-signin" role="form" action="home.php">
+        <h2 class="form-signin-heading">Cadastro de atividades dos docentes da UFFS</h2>
+        <input type="user" class="form-control" placeholder="Siape ou Email" required autofocus>
+        <input type="password" class="form-control" placeholder="Senha" required>
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Lembrar-me
+        </label>
+        <button class="btn btn-lg btn-success btn-block" type="submit">Continuar</button>
+      </form>
+
+    </div> <!-- /container -->
+
+	<div class="footer" id="id-footer">
+		  <a href="http://www.uffs.edu.br" target="blank">
+			<div class="container_logo_uffs" id="id-container_logo_uffs">
+			</div>
+		</a>
+		<p>Universidade Federal da Fronteira Sul </br> Ciência da Computação</p>
+	</div>	
+  </body>
+</html>
