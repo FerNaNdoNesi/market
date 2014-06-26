1d81e4c39b0babbbcc4ffe78566094a4ef517f64
Fernando Nesi <fernando.nesi@cresolsicoper.com.br>
2014-06-26 15:13:49 -0300

    adcionado 2 modelos de anúncio

--- dsv/index.php
+++ dsv/index.php
@@ -6,7 +6,6 @@
 <html lang="pt-br">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"> <!-- charset=utf-8">-->
-		<meta charset="utf-8"> <!-- charset="iso-8859-">-->
 		<title>Mercado Laranjeiras</title>
 		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -27,14 +26,14 @@
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><i class="glyphicon glyphicon-plus"></i> Criar An&uacute;ncio</a></li>
+            <li><a href="#"><i class="glyphicon glyphicon-plus"></i> Criar Anúncio</a></li>
             <li class="dropdown">
               <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#">
                 <i class="glyphicon glyphicon-user"></i> FerNaNdoNesi <span class="caret"></span>
               </a>
               <ul id="g-account-menu" class="dropdown-menu" role="menu">
                 <li><a href="#">Painel de controle</a></li>
-                <li><a href="#">Meus an&uacute;ncios</a></li>
+                <li><a href="#">Meus anúncios</a></li>
                 <li><a href="#">Sair</a></li>
               </ul>
             </li>
